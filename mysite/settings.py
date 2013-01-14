e83afc4f552d4eb50a16f9c49768df44e017a380
Nathan Crock <nathan@ip-10-243-123-27.ec2.internal>
2013-01-14 06:12:40 +0000

    file server overhaul

--- mysite/settings.py
+++ mysite/settings.py
@@ -4,7 +4,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Nathan Crock', 'nathan.crock@renegade-boxing.com'),
 )
 
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.split(__file__)[0], ".."))
@@ -15,9 +15,9 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'renegade',                      # Or path to database file if using sqlite3.
-        'USER': 'nathan',                      # Not used with sqlite3.
-        'PASSWORD': 'Mathisnumbuh1!',                  # Not used with sqlite3.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -31,18 +31,20 @@ SITE_ID = 1
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'mysite/media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'all_statics')
+DOWNLOAD_DIR = os.path.join( STATIC_ROOT, 'downloads' )
+DOWNLOAD_URL = '/download/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -51,7 +53,7 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-	os.path.join(SITE_DIR, 'static'),
+    os.path.join(SITE_DIR, 'static'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -66,7 +68,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = '3o@-^6@5j)xl5ltgl!(5y9=)9=8%#f(zyf&amp;k(j28@@21k_*2at'
+SECRET_KEY = ''
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
--- mysite/views.py
+++ mysite/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render_to_response
+from django.conf import settings
+from django.shortcuts import render, render_to_response
 from django.template import RequestContext
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
+from django.utils.encoding import smart_str
 from forms import ContactForm
+from os.path import isfile, join
 
 def home(request):
 	return render_to_response('home.html', context_instance=RequestContext(request))
+
+def download(request, file=None):
+	if file == None:
+		return HttpResponseNotFound('<h1>Sorry, that file does not exist!</h1>')
+
+	response = HttpResponse(my_data, content_type='static/base/media/'+file)
+	response['Content-Disposition'] = 'attachment; filename='+file
 
 def news(request):
 	return render_to_response('news.html', context_instance=RequestContext(request))
@@ -41,3 +51,13 @@ def members(request):
 
 def classes(request):
 	return render_to_response('classes.html', context_instance=RequestContext(request))
+
+def downloads(request, file_name):
+	dl_item = join( settings.DOWNLOAD_DIR, file_name )
+	if isfile( dl_item ):
+		response = HttpResponse(mimetype='application/force-download')
+		response['Content-Disposition'] = 'attachment; filename=%s' % smart_str(file_name)
+		response['X-Sendfile'] = smart_str( dl_item )
+		return response
+	else:
+		return HttpResponseRedirect('/home/') # Redirect after POST
